560036126f025f8c347a27bfba0421279834721a
azsheyeet <azrealyeno@gmail.com>
2026-02-28 17:27:06 +0800

    Integrated dev changes to feature/auth branch

--- server/src/firebase.js
+++ server/src/firebase.js
@@ -1,5 +1,13 @@
 const admin = require('firebase-admin');
 
+require('dotenv').config(); // load .env at the very top
+
+const privateKeyRaw = process.env.FIREBASE_PRIVATE_KEY;
+
+if (!privateKeyRaw) {
+  throw new Error("FIREBASE_PRIVATE_KEY is missing in your environment variables!");
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
--- server/src/index.js
+++ server/src/index.js
@@ -33,3 +33,7 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');  // simple message
+});
